52281eb2c1b284d1dfef96f74bdbe9c81f3d427f
MBenedicte <mbenedicte20@gmail.com>
2020-01-22 15:01:07 +0200

    Configure travis

--- src/tests/index.js
+++ src/tests/index.js
@@ -6,9 +6,10 @@ const { expect } = chai;
 chai.use(chaiHttp);
 describe('Get ', () => {
   it('should start the first method', () => {
+    console.log(app);
     chai
       .request(app)
-      .get('/api/v1')
+      .get('/api/v1/')
       .end((err, res) => {
         expect(res.status).to.equal(200);
       });
